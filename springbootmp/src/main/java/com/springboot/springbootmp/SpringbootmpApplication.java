8a3801bf01603ae311b881c5cebb39f1aa13361d
liulie <763838587@qq.com>
2018-12-12 19:14:36 +0800

    首次提交

---
+++ springbootmp/src/main/java/com/springboot/springbootmp/SpringbootmpApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.springbootmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmpApplication.class, args);
+	}
+}
